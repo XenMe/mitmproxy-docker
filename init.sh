a8248f2d7a5443b02e6471d68cad7daba5463b5a
xenme <froyo@xenme.com>
2018-08-11 21:32:16 +0800

    COPY init.sh and add default parameters

--- init.sh
+++ init.sh
@@ -6,4 +6,8 @@ MITMPROXY_PATH="/home/mitmproxy/.mitmproxy"
 mkdir -p "$MITMPROXY_PATH"
 chown -R mitmproxy:mitmproxy "$MITMPROXY_PATH"
 
-su-exec mitmproxy mitmproxy
+su-exec mitmproxy mitmproxy \
+    -m transparent \
+    -s adblock.py \
+    --set stream_large_bodies=500k \
+    --set ssl_insecure=true
